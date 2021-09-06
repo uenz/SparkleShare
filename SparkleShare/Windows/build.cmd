1afe29d80386df1d763586e6302132545bec59f6
uenz <uenz@gmx.net>
2021-09-06 14:22:59 +0200

    Update to msi building

--- SparkleShare/Windows/build.cmd
+++ SparkleShare/Windows/build.cmd
@@ -14,9 +14,13 @@ if "%1"=="installer" (
 	if exist "%wixBinDir%" (
 	  if exist "%~dp0\SparkleShare.msi" del "%~dp0\SparkleShare.msi"
 		"%wixBinDir%\heat.exe" dir "%~dp0\..\..\bin\msysgit" -cg msysGitComponentGroup -gg -scom -sreg -sfrag -srd -dr MSYSGIT_DIR -var wix.msysgitpath -o "%~dp0\msysgit.wxs"
+		"%wixBinDir%\heat.exe" dir "%~dp0\..\..\bin\Images"  -cg ImagesComponentGroup  -gg -scom -sreg -sfrag -srd -dr IMAGES_DIR  -var wix.imagespath  -o "%~dp0\images.wxs"
+		"%wixBinDir%\heat.exe" dir "%~dp0\..\..\bin\Presets" -cg PresetsComponentGroup -gg -scom -sreg -sfrag -srd -dr PRESETS_DIR -var wix.presetspath -o "%~dp0\presets.wxs"
 		"%wixBinDir%\candle" "%~dp0\SparkleShare.wxs" -ext WixUIExtension -ext WixUtilExtension -o "%~dp0\"
 		"%wixBinDir%\candle" "%~dp0\msysgit.wxs" -ext WixUIExtension -ext WixUtilExtension -o "%~dp0\"
-		"%wixBinDir%\light" -ext WixUIExtension -ext WixUtilExtension "%~dp0\Sparkleshare.wixobj" "%~dp0\msysgit.wixobj" -droot="%~dp0\..\.." -dmsysgitpath="%~dp0\..\..\bin\msysgit" -dpluginsdir="%~dp0\..\..\bin\plugins"  -o "%~dp0\SparkleShare.msi"
+		"%wixBinDir%\candle" "%~dp0\images.wxs" -ext WixUIExtension -ext WixUtilExtension -o "%~dp0\"
+		"%wixBinDir%\candle" "%~dp0\presets.wxs" -ext WixUIExtension -ext WixUtilExtension -o "%~dp0\"
+		"%wixBinDir%\light" -ext WixUIExtension -ext WixUtilExtension "%~dp0\Sparkleshare.wixobj" "%~dp0\msysgit.wixobj" "%~dp0\images.wixobj" "%~dp0\presets.wixobj" -droot="%~dp0\..\.." -dmsysgitpath="%~dp0\..\..\bin\msysgit" -dimagespath="%~dp0\..\..\bin\Images"  -dpresetspath="%~dp0\..\..\bin\Presets" -o "%~dp0\SparkleShare.msi"
 		if exist "%~dp0\SparkleShare.msi" echo SparkleShare.msi created.
 	) else (
 		echo Not building installer ^(could not find wix, Windows Installer XML toolset^)
