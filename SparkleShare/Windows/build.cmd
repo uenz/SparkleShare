7e376497343bc15e87acf4a808d6828909723a0a
uenz <uenz@gmx.net>
2024-10-18 21:25:09 +0200

    Added ScpUri tests

--- SparkleShare/Windows/build.cmd
+++ SparkleShare/Windows/build.cmd
@@ -1,14 +1,14 @@
 @echo off
 
-set WinDirNet=%WinDir%\Microsoft.NET\Framework
-set msbuild="%WinDirNet%\v4.0\msbuild.exe"
-if not exist %msbuild% set msbuild="%WinDirNet%\v4.0.30319\msbuild.exe"
+REM set WinDirNet=%WinDir%\Microsoft.NET\Framework
+REM set msbuild="%WinDirNet%\v4.0\msbuild.exe"
+REM if not exist %msbuild% set msbuild="%WinDirNet%\v4.0.30319\msbuild.exe"
 set WIX=C:\Program Files (x86)\WiX Toolset v3.14
 set wixBinDir=%WIX%\bin
 set OutputDir=%~dp0bin
 if not exist "%OutputDir%" mkdir "%OutputDir%"
 
-%msbuild% "%~dp0..\..\SparkleShare.sln" /target:SparkleShare_Windows:Rebuild /p:Configuration=ReleaseWindows /p:Platform="Any CPU" -m
+dotnet build "%~dp0..\..\SparkleShare.sln" /target:SparkleShare_Windows:Rebuild /p:Configuration=ReleaseWindows /p:Platform="Any CPU" -m
 
 if "%1"=="installer" (
 	if exist "%wixBinDir%" (
@@ -16,11 +16,13 @@ if "%1"=="installer" (
 		"%wixBinDir%\heat.exe" dir "%OutputDir%\git_scm" -cg gitScmComponentGroup -gg -scom -sreg -sfrag -srd -dr GITSCM_DIR -var wix.gitscmpath -o "%~dp0\git_scm.wxs"
 		"%wixBinDir%\heat.exe" dir "%OutputDir%\Images"  -cg ImagesComponentGroup  -gg -scom -sreg -sfrag -srd -dr IMAGES_DIR  -var wix.imagespath  -o "%~dp0\images.wxs"
 		"%wixBinDir%\heat.exe" dir "%OutputDir%\Presets" -cg PresetsComponentGroup -gg -scom -sreg -sfrag -srd -dr PRESETS_DIR -var wix.presetspath -o "%~dp0\presets.wxs"
+		"%wixBinDir%\heat.exe" dir "%OutputDir%\runtimes" -cg runtimesComponentGroup -gg -scom -sreg -sfrag -srd -dr PRESETS_DIR -var wix.runtimespath -o "%~dp0\runtimes.wxs"
 		"%wixBinDir%\candle" "%~dp0\SparkleShare.wxs" -ext WixUIExtension -ext WixUtilExtension -o "%~dp0\"
 		"%wixBinDir%\candle" "%~dp0\git_scm.wxs" -ext WixUIExtension -ext WixUtilExtension -o "%~dp0\"
 		"%wixBinDir%\candle" "%~dp0\images.wxs" -ext WixUIExtension -ext WixUtilExtension -o "%~dp0\"
 		"%wixBinDir%\candle" "%~dp0\presets.wxs" -ext WixUIExtension -ext WixUtilExtension -o "%~dp0\"
-		"%wixBinDir%\light" -ext WixUIExtension -ext WixUtilExtension "%~dp0Sparkleshare.wixobj" "%~dp0git_scm.wixobj" "%~dp0images.wixobj" "%~dp0presets.wixobj" -droot="%~dp0." -dgitscmpath="%OutputDir%\git_scm" -dimagespath="%OutputDir%\Images"  -dpresetspath="%OutputDir%\Presets" -o "%~dp0SparkleShare.msi"
+		"%wixBinDir%\candle" "%~dp0\runtimes.wxs" -ext WixUIExtension -ext WixUtilExtension -o "%~dp0\"
+		"%wixBinDir%\light" -ext WixUIExtension -ext WixUtilExtension "%~dp0Sparkleshare.wixobj" "%~dp0git_scm.wixobj" "%~dp0images.wixobj" "%~dp0presets.wixobj" "%~dp0runtimes.wixobj" -droot="%~dp0." -dgitscmpath="%OutputDir%\git_scm" -dimagespath="%OutputDir%\Images"  -dpresetspath="%OutputDir%\Presets" -druntimespath="%OutputDir%\runtimes" -o "%~dp0SparkleShare.msi"
 		if exist "%~dp0\SparkleShare.msi" echo SparkleShare.msi created.
 	) else (
 		echo Not building installer ^(could not find wix, Windows Installer XML toolset^)
---
+++ Sparkles/Tests/TestScpUri.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparkles.Tests
+{
+    [TestFixture()]
+    internal class TestScpUri
+    {
+        static string urlString = "git@github.com:uenz/SparkleShare.git";
+        static ScpUri scp_url = new(urlString);
+        static ScpUri url = new(urlString,false);
+
+        [Test()]
+        public void ScpUriToString()
+        {
+            //string urlString = "git@github.com:uenz/SparkleShare.git";
+            //ScpUri scp_url= new(urlString);
+            //ScpUri url = new(urlString,false);
+
+            Assert.That(TestScpUri.scp_url.ToString()== TestScpUri.urlString);
+            Assert.That(TestScpUri.url.ToString() == TestScpUri.url.Scheme+"://"+ TestScpUri.urlString.Replace(':','/'));
+        }
+        [Test()]
+        public void SshUriToString()
+        {
+            string urlString = "ssh://git@github.com/uenz/SparkleShare.git";
+            ScpUri scp_url = new(urlString);
+            ScpUri url = new(urlString, false);
+
+            Assert.That(scp_url.ToString() == urlString);
+            Assert.That(url.ToString() == urlString);
+        }
+        [Test()]
+        public void HttpToString()
+        {
+            string urlString = "http://git@github.com/uenz/SparkleShare.git";
+            ScpUri scp_url = new(urlString);
+            ScpUri url = new(urlString, false);
+
+            Assert.That(scp_url.ToString() == urlString);
+            Assert.That(url.ToString() == urlString);
+        }
+        [Test()]
+        public void AbsolutePath()
+        {
+            Assert.That(TestScpUri.url.AbsolutePath == "/uenz/SparkleShare.git");
+            Assert.That(TestScpUri.scp_url.AbsolutePath == ":uenz/SparkleShare.git");
+
+        }
+        [Test()]
+        public void Scheme()
+        {
+            Assert.That(TestScpUri.url.Scheme == TestScpUri.scp_url.Scheme);
+        }
+        [Test()]
+        public void Host()
+        {
+            Assert.That(TestScpUri.url.Host == TestScpUri.scp_url.Host);
+        }
+        [Test()]
+        public void Userinfo()
+        {
+            Assert.That(TestScpUri.url.UserInfo == TestScpUri.scp_url.UserInfo);
+        }
+    }
+}
