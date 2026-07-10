aad18a3d218180ba2ad2611a6c01f06b078dceef
uenz <uenz@gmx.net>
2026-07-10 21:23:24 +0200

    macos build log minimal

--- SparkleShare/Avalonia/os_specific/Windows/build.cmd
+++ SparkleShare/Avalonia/os_specific/Windows/build.cmd
@@ -5,6 +5,6 @@ dotnet build "%~dp0..\..\..\..\SparkleShare.sln" /target:SparkleShare_Avalonia:R
 
 if "%1"=="installer" (
 	dotnet restore "%~dp0..\..\..\..\SparkleShare.sln"
-	dotnet build "%~dp0..\..\..\..\SparkleShare.sln" /target:SparkleShare_Avalonia_Installer:Rebuild /p:Configuration=ReleaseAvalonia /p:Platform="Any CPU" -m -v:detailed	
+	dotnet build "%~dp0..\..\..\..\SparkleShare.sln" /target:SparkleShare_Avalonia_Installer:Rebuild /p:Configuration=ReleaseAvalonia /p:Platform="Any CPU" -m -v:minimal	
 ) else echo Not building installer, as it was not requested. ^(Issue "build.cmd installer" to build installer ^)
 
