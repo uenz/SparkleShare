30e407dfdf724ba81fd9e75a2040e1f39a3681c8
uenz <uenz@gmx.net>
2026-08-27 23:15:27 +0200

    Update build.sh

--- SparkleShare/Avalonia/os_specific/MacOS/build.sh
+++ SparkleShare/Avalonia/os_specific/MacOS/build.sh
@@ -1,4 +1,4 @@
 export runtimeidentifier="${1:-x64}"
 export projectFolder="$(dirname "$(realpath "$0")")"
 echo "${projectFolder}"
-dotnet publish "${projectFolder}/../../../../SparkleShare.sln" /target:SparkleShare_Avalonia:Rebuild /p:Configuration=ReleaseAvalonia /p:Platform="Any CPU" -m -v:minimal /p:SelfContained=true /p:RuntimeIdentifier="osx-${runtimeidentifier}" 
+dotnet publish "${projectFolder}/../../../../SparkleShare.sln" /target:SparkleShare_Avalonia /p:Configuration=ReleaseAvalonia /p:Platform="Any CPU" -m -v:minimal /p:SelfContained=true /p:RuntimeIdentifier="osx-${runtimeidentifier}" 
